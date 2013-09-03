3f68ec459863c6b6cd89b7ea7e6459869416ed31
Josch600@gmail.com <Josch600@gmail.com@fc7da526-431c-80fe-3c8c-c148ff18d7ef>
2013-09-03 09:59:27 +0000

    First V2.x release

---
+++ tags/fowsr-2.0/fowsr.src/pywws.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Call fowsr and upload data to pywws
+
+wsr="/usr/bin/fowsr -fp"
+LOG=/var/pywws.log
+UID=$1
+PWD=$2
+server=www.pywws.com
+
+WPUT="$LOG ftp://$UID:$PWD@$server"
+
+rm -f $LOG
+$wsr
+
+wput $WPUT
